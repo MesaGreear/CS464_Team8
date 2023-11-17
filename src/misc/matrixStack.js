de6455e577ca184d7ef932d7e9f436f866f91bfa
Mesa Greear <mesagreear@u.boisestate.edu>
2023-11-17 00:39:17 -0700

    Improved Class declarations & documentation

--- src/misc/matrixStack.js
+++ src/misc/matrixStack.js
@@ -1,97 +1,99 @@
-
 /**
- * Constructor: Initialize an empty matrix stack.
+ * Used to create MStack objects. A MStack, or Matrix Stack, contains a stack of 4x4 matrices
+ * the top of which can be modified in a variety of ways. When used properly, a Matrix Stack
+ * aids in transforming, rotating, or scaling a drawn object in relation to another drawn object.
  */
-function MStack() { this.stack = [mat4.identity(mat4.create())]; this.saved = null; }
+class MStack {
+    /**
+     * Constructor: Initialize an empty matrix stack.
+     */
+    constructor() { this.stack = [mat4.identity(mat4.create())]; this.saved = null; }
 
-/**
- * Pop and return the top of the matrix stack.
- * 
- * @returns the popped stack
- */
-MStack.prototype.pop = function() {
-    if(this.stack.length < 1)
-        throw "Invalid Matrix Stack!!!!!";
-    return this.stack.pop();
+    /**
+     * Pop and return the top of the matrix stack.
+     * 
+     * @returns the popped matrix
+     */
+    pop() {
+        if(this.stack.length < 1)
+            throw "Invalid Matrix Stack!!!!!";
+        return this.stack.pop();
+    }
+
+    /**
+     * Create a copy of the top of the stack and push it to the top.
+     */
+    push() { this.stack.push(this.getMatrix()); }
+
+    /**
+     * @returns the top of the stack
+     */
+    getMatrix() { return this.stack[this.stack.length - 1].slice(); }
+
+    /**
+     * @param {mat4} m matrix to set the top of the stack to
+     */
+    setMatrix(m) { this.stack[this.stack.length - 1] = m; }
+
+    /**
+     * Translate the top of the stack by the given coordinates.
+     * 
+     * @param {vec3} coord coordinates representing xyz in 3D space
+     */
+    translate(coord) { this.setMatrix(mat4.translate(this.getMatrix(), coord)); }
+
+    /**
+     * Rotate the top of the stack by the given degrees in the given direction.
+     * 
+     * @param {vec3} axis  the axis to rotate on
+     * @param {int} degree degrees to rotate by
+     */
+    rotate(axis, degree) { this.setMatrix(mat4.rotate(this.getMatrix(), degree/180.0*3.1415, axis)); }
+
+    /**
+     * Rotate the top of the stack by the given degrees on the X-axis.
+     * 
+     * @param {int} degree degrees tp rotate by
+     */
+    rotateX(degree) { this.rotate([1,0,0], degree); }
+
+    /**
+     * Rotate the top of the stack by the given degrees on the Y-axis.
+     * 
+     * @param {int} degree degrees tp rotate by
+     */
+    rotateY(degree) { this.rotate([0,1,0], degree); }
+
+    /**
+     * Rotate the top of the stack by the given degrees on the Z-axis.
+     * 
+     * @param {int} degree degrees tp rotate by
+     */
+    rotateZ(degree) { this.rotate([0,0,1], degree); }
+
+    /**
+     * Scale the top of the stack by the given scale.
+     * 
+     * @param {vec3 | int} scale vec3 to scale each axis individually or int to scale
+     *                           all axes by the given int
+     */
+    scale(scale) {
+        if(Array.isArray(scale))
+            this.setMatrix(mat4.scale(this.getMatrix(), scale));
+        else
+            this.setMatrix(mat4.scale(this.getMatrix(), [scale, scale, scale]));
+    }
+
+    /**
+     * Save the current state of the top of the stack. Can be restored later by restore().
+     * Can be used to undo changes that are specific to a drawn object after it has been
+     * drawn.
+     */
+    save() { this.saved = this.getMatrix(); }
+
+    /**
+     * Restore the previously saved state, undoing any changes that have been made since
+     * the last save().
+     */
+    restore() { this.setMatrix(this.saved); }
 }
-
-/**
- * Create a copy of the top of the stack and push it to the top.
- */
-MStack.prototype.push = function() { this.stack.push(this.getMatrix()); }
-
-/**
- * Get (without removing/popping) the top of the stack.
- * 
- * @returns the top of the stack
- */
-MStack.prototype.getMatrix = function() { return this.stack[this.stack.length - 1].slice(); }
-
-/**
- * Set the top of the stack to the given matrix.
- * 
- * @param {mat4} m matrix to set the top of the stack to
- */
-MStack.prototype.setMatrix = function(m) { this.stack[this.stack.length - 1] = m; }
-
-/**
- * Translate the top of the stack by the given coordinates.
- * 
- * @param {vec3} coord coordinates representing xyz in 3D space
- */
-MStack.prototype.translate = function(coord) { this.setMatrix(mat4.translate(this.getMatrix(), coord)); }
-
-/**
- * Rotate the top of the stack by the given degrees in the given direction.
- * 
- * @param {vec3} axis  the axis to rotate on
- * @param {int} degree degrees to rotate by
- */
-MStack.prototype.rotate = function(axis, degree) { this.setMatrix(mat4.rotate(this.getMatrix(), degree/180.0*3.1415, axis)); }
-
-/**
- * Rotate the top of the stack by the given degrees on the X-axis.
- * 
- * @param {int} degree degrees tp rotate by
- */
-MStack.prototype.rotateX = function(degree) { this.rotate([1,0,0], degree); }
-
-/**
- * Rotate the top of the stack by the given degrees on the Y-axis.
- * 
- * @param {int} degree degrees tp rotate by
- */
-MStack.prototype.rotateY = function(degree) { this.rotate([0,1,0], degree); }
-
-/**
- * Rotate the top of the stack by the given degrees on the Z-axis.
- * 
- * @param {int} degree degrees tp rotate by
- */
-MStack.prototype.rotateZ = function(degree) { this.rotate([0,0,1], degree); }
-
-/**
- * Scale the top of the stack by the given scale.
- * 
- * @param {vec3 | int} scale vec3 to scale each axis individually or int to scale
- *                           all axes by the given int
- */
-MStack.prototype.scale = function(scale) {
-    if(Array.isArray(scale))
-        this.setMatrix(mat4.scale(this.getMatrix(), scale));
-    else
-        this.setMatrix(mat4.scale(this.getMatrix(), [scale, scale, scale]));
-}
-
-/**
- * Save the current state of the top of the stack. Can be restored later by restore().
- * Can be used to undo changes that are specific to a drawn object after it has been
- * drawn.
- */
-MStack.prototype.save = function() { this.saved = this.getMatrix(); }
-
-/**
- * Restore the previously saved state, undoing any changes that have been made since
- * the last save().
- */
-MStack.prototype.restore = function() { this.setMatrix(this.saved); }
--- src/misc/pipe.js
+++ src/misc/pipe.js
@@ -1,86 +1,130 @@
+
 /**
- * Constructor: Initialize a pipe (just a sphere starting out) at a random coordinate.
- * 
- * @param {GL_TEXTURE_2D} tex The texture of this pipe
+ * Used to create Pipe objects. A Pipe object represents a pipe/tube that can be drawn and
+ * contains info necessary to draw said pipe. This information includes a queue of PipeSegments,
+ * the remaining length of the current section being drawn, the direction the current section is
+ * being drawn in, and the texture of the pipe.
  */
-function Pipe(tex) { 
-    this.queue = [];
-    this.queue.push(new PipeSegment([randInt(10, 80), randInt(-40, -10), randInt(-80, -10)], 1, 0));
-    this.genNewSectionLength();
-    this.dir = randInt(0, 6);
-    this.tex = tex;
-}
-
-Pipe.prototype.addSegment = function() {
-    // if a section has been fully drawn...
-    if(this.length <= 0) {
-        // calculate a new length for this pipe section
+class Pipe {
+    
+    /**
+     * Constructor: Initialize a pipe (just a sphere starting out) at a random coordinate. The
+     * section length and direction will be randomly determined.
+     * 
+     * @param {GL_TEXTURE_2D} tex The texture of this pipe
+     */
+    constructor (tex) {
+        this.queue = [];
+        this.queue.push(new PipeSegment([randInt(10, 80), randInt(-40, -10), randInt(-80, -10)], 1, 0));
         this.genNewSectionLength();
+        this.dir = randInt(0, 6);
+        this.tex = tex;
+    }
 
-        // initialize a sphere segment
-        this.initNextSegment(1, 0);
+    /**
+     * Add a new PipeSegment to this pipe. If the section being currently drawn is complete, then
+     * a new section going in a new valid direction is determined.
+     */
+    addSegment() {
+        // if a section has been fully drawn...
+        if(this.length <= 0) {
+            // calculate a new length for this pipe section
+            this.genNewSectionLength();
 
-        // generate the 4 valid directions based on the last direction we were drawing in
-        var dirs = [];
-        if(this.dir == 0 || this.dir == 1)      // X-axis
-            dirs.push(2, 3, 4, 5);
-        else if(this.dir == 2 || this.dir == 3) // Y-axis
-            dirs.push(0, 1, 4, 5);
-        else                                    // Z-axis
-            dirs.push(0, 1, 2, 3);
+            // initialize a sphere segment
+            this.initNextSegment(1);
 
-        // randomly select a valid direction and check that when the entire section is drawn, the
-        // resulting section will be inbounds. If it isn't, try again till a valid direction is found
-        var inBounds = false;
-        var currPos = this.getLastSegment().coord.slice();
+            // generate the 4 valid directions based on the last direction we were drawing in
+            var dirs = [];
+            if(this.dir == 0 || this.dir == 1)      // X-axis
+                dirs.push(2, 3, 4, 5);
+            else if(this.dir == 2 || this.dir == 3) // Y-axis
+                dirs.push(0, 1, 4, 5);
+            else                                    // Z-axis
+                dirs.push(0, 1, 2, 3);
 
-        while(!inBounds) {
-            this.dir = dirs[Math.floor(Math.random() * 4)];
-            switch(this.dir) {
-                case 0:
-                    inBounds = currPos[0] - this.length > 0;   break;
-                case 1:
-                    inBounds = currPos[0] + this.length < 90;  break;
-                case 2:
-                    inBounds = currPos[1] - this.length > -50; break;
-                case 3:
-                    inBounds = currPos[1] + this.length < 0;   break;
-                case 4:
-                    inBounds = currPos[2] - this.length > -90; break;
-                case 5:
-                    inBounds = currPos[2] + this.length < 0;
+            // randomly select a valid direction and check that when the entire section is drawn, the
+            // resulting section will be inbounds. If it isn't, try again till a valid direction is found
+            var inBounds = false;
+            var currPos = this.getLastSegment().coord.slice();
+
+            while(!inBounds) {
+                this.dir = dirs[Math.floor(Math.random() * 4)];
+                switch(this.dir) {
+                    case 0:
+                        inBounds = currPos[0] - this.length > 0;   break;
+                    case 1:
+                        inBounds = currPos[0] + this.length < 90;  break;
+                    case 2:
+                        inBounds = currPos[1] - this.length > -50; break;
+                    case 3:
+                        inBounds = currPos[1] + this.length < 0;   break;
+                    case 4:
+                        inBounds = currPos[2] - this.length > -90; break;
+                    case 5:
+                        inBounds = currPos[2] + this.length < 0;
+                }
             }
         }
+        // else just initialize the next cylinder segment
+        else {
+            this.initNextSegment(2);
+        }
     }
-    // else just initialize the next cylinder segment
-    else {
-        this.initNextSegment(2, Math.floor(this.dir/2));
+
+    /**
+     * Calculate the next position for a new segment and then initialize and add the PipeSegment
+     * to the queue.
+     * 
+     * This method should not typically be used outside the Pipe class, consider a private utility
+     * method.
+     * 
+     * @param {int} shape An index corelating to a shape in the buffers' arrays
+     */
+    initNextSegment(shape) {
+        var newPos = this.getLastSegment().coord.slice();
+        newPos[Math.floor(this.dir/2)] += (this.dir % 2 == 0 ? -1 : 1);
+        this.length--;
+        
+        this.queue.push(new PipeSegment(newPos, shape, Math.floor(this.dir/2)));
     }
+
+    /**
+     * Trim the front of the queue till it's length is equal to or less than the given length.
+     * 
+     * @param {int} length The desired max length of the queue.
+     */
+    trimSegments(length) { while(this.queue.length > length) this.queue.shift(); }
+
+    /**
+     * Set the length of the next/new section to a random value within certain bounds.
+     */
+    genNewSectionLength() { this.length = randInt(5, 20); }
+
+    /**
+     * @returns The last PipeSegment in the queue.
+     */
+    getLastSegment() { return this.queue[this.queue.length - 1] }
 }
-
-Pipe.prototype.initNextSegment = function (shape, dir) {
-    var newPos = this.getLastSegment().coord.slice();
-    newPos[Math.floor(this.dir/2)] += (this.dir % 2 == 0 ? -1 : 1);
-    this.length--;
-    
-    this.queue.push(new PipeSegment(newPos, shape, dir));
-}
-
-Pipe.prototype.trimSegments = function (length) { while(this.queue.length > length) this.queue.shift(); }
-
-Pipe.prototype.genNewSectionLength = function() { this.length = randInt(5, 20); }
-
-Pipe.prototype.getLastSegment = function () {return this.queue[this.queue.length - 1]};
 
 /**
- * Constructor: 
- * 
- * @param {*} coord 
- * @param {*} shape 
- * @param {*} dir 
+ * Used to create PipeSegment Objects. A PipeSegment object is a simple datatype that represents
+ * a single segment of a pipe. A PipeSegment contains it's coordinates in 3D space, it's shape,
+ * and the direction of the section it is a part of.
  */
-function PipeSegment(coord, shape, dir) {
-    this.coord = coord;
-    this.shape = shape;
-    this.dir = dir;
+class PipeSegment {
+
+    /**
+     * Constructor: Initialize a PipeSegment Object with the given values.
+     * 
+     * @param {vec3}      coord The coordinates of this PipeSegment in 3D space
+     * @param {int}       shape An index corelating to a shape in the buffers' arrays
+     * @param {1 | 2 | 3} dir   The direction of the section this PipeSegment is a part of (0 = X-axis,
+     *                          1 = Y-axis, 2 = Z-axis)
+     */
+    constructor(coord, shape, dir) {
+        this.coord = coord;
+        this.shape = shape;
+        this.dir = dir;
+    }
 }
